c7afbc2646ee22d88ca0236d866cb351925af9a9
pzayats <pavlo.zayats@digitalturbine.com>
2025-01-26 23:33:43 +0200

    debug

--- src/app/actions/app-instance.ts
+++ src/app/actions/app-instance.ts
@@ -1,5 +1,6 @@
 import { decodeJwt } from '@/app/utils/jwt-verify';
 
 export const parseAccessToken = (token: string) => {
+  console.log('TOKEN 1: ' + token);
   return decodeJwt(token.replace(/.*JWS\./, ''));
 };
--- src/app/api/webhooks/v1/order/route.ts
+++ src/app/api/webhooks/v1/order/route.ts
@@ -2,7 +2,7 @@ import { wixAppClient } from '@/app/utils/wix-sdk.app';
 import { type NextRequest } from 'next/server';
 
 export async function POST(request: NextRequest) {
-  console.info('Webhook::order accepted - called');
+  console.info('Webhook::order accepted - called. Input: ' + JSON.stringify(request));
   const { eventType, instanceId, payload } = await wixAppClient.webhooks.processRequest(request);
 
   console.info('Webhook::order accepted - input is:', {
