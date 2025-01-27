42e1a6ca3a906cd3e5266be52e5239a199fe187b
pzayats <pavlo.zayats@digitalturbine.com>
2025-01-27 08:50:02 +0200

    fix

--- src/app/utils/jwt-verify.ts
+++ src/app/utils/jwt-verify.ts
@@ -2,6 +2,7 @@ import jwt, { Jwt, JwtPayload } from 'jsonwebtoken';
 
 // Decode the JWT token without secret key verification.
 export const decodeJwt = <T = any>(token: string): T | null => {
+  console.log('decodeJwt: ' + token);
   const result = jwt.decode(token, { complete: true }) as Jwt;
   const payload = result.payload as JwtPayload;
   if (payload.exp! < Math.floor(Date.now() / 1000) && payload.iat! > Math.floor(Date.now() / 1000)) {
