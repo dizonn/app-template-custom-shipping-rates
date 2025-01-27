2aca34be6a409f649aef557b7f5f6ea376ce1702
pzayats <pavlo.zayats@digitalturbine.com>
2025-01-27 02:09:41 +0200

    fix

--- src/app/api/webhooks/v1/order/route.ts
+++ src/app/api/webhooks/v1/order/route.ts
@@ -1,6 +1,8 @@
 import { wixAppClient } from '@/app/utils/wix-sdk.app';
 import { type NextRequest } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(request: NextRequest) {
   const body = await request.text();
   console.log('Body here:' + body);
